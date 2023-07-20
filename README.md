9b41e867e77952fef515206779586d6e790e15a0
zeehamza <zeehamza49@gmail.com>
2023-07-20 16:04:39 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## Project setup
+
+## FrontEnd
+
+```
+yarn install
+```
+
+### Compiles and minifies for production
+
+```
+yarn build
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn dev
+```
+
+## Backend
+
+```
+cd server
+```
+
+```
+composer install
+```
+
+### Compiles and minifies for production
+
+```
+symfony server:start
+```
---
+++ server/src/Controller/ProductController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProductController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/api/products',  methods:['GET'])]
+    public function getProducts(): JsonResponse
+    {
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+        $data = [];
+        foreach ($products as $product) {
+            $data[] = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'price' => $product->getPrice(),
+                'description' => $product->getDescription(),
+            ];
+        }
+
+        return new JsonResponse($data);
+    }
+
+    #[Route('/api/products', methods:['POST'])]
+    public function saveProduct(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $product = new Product();
+        $product->setName($data['name']);
+        $product->setPrice($data['price']);
+        $product->setDescription($data['description']);
+
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        return new JsonResponse(['message' => 'Product saved successfully'], Response::HTTP_CREATED);
+    }
+}
+
